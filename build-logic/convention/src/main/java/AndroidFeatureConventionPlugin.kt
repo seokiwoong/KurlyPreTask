d76c795fa6609c0833719f35f383158c2e463e98
seokw1980 <seokw1980@gmail.com>
2024-03-23 23:59:20 +0900

    [build] build-logic 적용과 동시에 과제 pdf 보고 사용해야 할 라이브러리 우선 추가해 놓았음. 하다가 추가해야 할 사항 나오면 업데이트 필요.

---
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,26 @@
+import com.kurly.preTask.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("kurly.android.library")
+                apply("kurly.android.hilt")
+            }
+
+            dependencies {
+//                add("implementation", project(":core:ui"))
+//                add("implementation", project(":core:designsystem"))
+
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+
+                add("androidTestImplementation", libs.findLibrary("androidx.lifecycle.runtimeTesting").get())
+            }
+        }
+    }
+}
