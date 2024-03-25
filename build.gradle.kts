53e791d1345af98848ef7619431531c1daeb7ebe
seokw1980 <seokw1980@gmail.com>
2024-03-25 20:02:03 +0900

    mockserver 모듈 추가.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
 }
---
+++ core/mockserver/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kurly.android.library)
+    alias(libs.plugins.kurly.android.hilt)
+}
+
+android {
+    namespace = "com.kurly.pretask.core.mockserver"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+}
+
+dependencies {
+    implementation( "com.squareup.okhttp3:okhttp:4.12.0")
+    implementation( "com.google.code.gson:gson:2.10.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    ksp(libs.hilt.compiler)
+    kspTest(libs.hilt.compiler)
+    testImplementation(libs.hilt.android.testing)
+    androidTestImplementation(libs.hilt.android.testing)
+}
