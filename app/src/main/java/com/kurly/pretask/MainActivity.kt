ef43f1c653d27c591ac114a4715a5e511b0408d2
seokw1980 <seokw1980@gmail.com>
2024-03-31 00:21:55 +0900

    [Fix] 안쓰는 mainViewModel 제거.

--- app/src/main/java/com/kurly/pretask/MainActivity.kt
+++ app/src/main/java/com/kurly/pretask/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.kurly.feature.main.MainScreen
 import com.kurly.pretask.designsystem.theme.KurlyPreTaskTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +21,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KurlyPreTaskTheme {
-                val mainViewModel: MainViewModel = hiltViewModel()
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
